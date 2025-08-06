990eac2d48c86e17409ce30701da90effe4e1298
shraddhac0206 <shraddhachauhan261999@gmail.com>
2025-08-06 17:11:35 -0500

    Add deployment scripts and update vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ import react from '@vitejs/plugin-react'
 // Update this block 👇
 export default defineConfig({
   plugins: [react()],
-  base: '/BA-Portfolio/', // <-- Add this line to match your repo name
+  base: '/BA-Portfolio/', // <-- Update this to match your GitHub repo name
   server: {
     port: 3000,
     open: true
