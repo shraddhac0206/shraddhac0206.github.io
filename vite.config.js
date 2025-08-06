2aaab77af35cbde714f3585d3e34d543aedc1b61
shraddhac0206 <shraddhachauhan261999@gmail.com>
2025-08-06 15:23:41 -0500

    Second commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true
+//   }
+// }) 
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Update this block 👇
 export default defineConfig({
   plugins: [react()],
+  base: '/BA-Portfolio/', // <-- Add this line to match your repo name
   server: {
     port: 3000,
     open: true
   }
-}) 
+})
